4d6cb0f2f9146657a519e1686c4633546ab9e6e4
babaliaris <babaliaris.nikos@gmail.com>
2023-11-13 17:24:40 +0200

    Installed react-hook-form, react-i18n, fontawesome-free

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import "normalize.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css'
 
 import {krRouter} from "src/root/router";
